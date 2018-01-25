8075e1713bb844d0d416e9c69481d9cb498dd84f
vcordero07 <vcordero07@gmail.com>
2018-01-24 23:26:19 -0500

    wip

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,24 @@
 import {ADD_ITEM, UPDATE_ITEM, DELETE_ITEM} from '../actions';
 
-const initialState = {
-    items: []
-};
 
-export const crudReducer = (state=initialState, action) => {
-    // Add code which handles each action here
+
+export const crudReducer = (state= { items: [] }, action) => {
     if (action.type === ADD_ITEM) {
+        return Object.assign({}, state, {
+            items: [...state.items, action.item]
+        });
     }
     else if (action.type === UPDATE_ITEM) {
+        return Object.assign({}, state, {
+            items: state.items.map(item =>
+                item.id === action.item.id ? action.item : item
+            )
+        });
     }
     else if (action.type === DELETE_ITEM) {
+        return Object.assign({}, state, {
+            items: state.items.filter(item => item.id !== action.item.id)
+        });
     }
     return state;
 };
